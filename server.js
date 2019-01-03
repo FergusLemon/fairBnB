8d276f214e69ecf57e3279e746d50688c8c4ab8e
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-03 13:25:20 +0000

    File set up - install body-parser and mongoose and set up basic express server file.

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.status(200).send("Connection test.");
+});
+
+app.listen(3000, function() {
+  console.log("Server listening on port", 3000)
+});
