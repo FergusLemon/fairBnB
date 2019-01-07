bbfe18ded8efaae26046f17e43edaac50e49e2f3
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-07 13:27:39 +0000

    Add user registration page and create tests to check that the homepage has a link for sign up and that it takes new users to the registration page.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var app = require('./app');
 var port = process.env.PORT || 3000;
 var path = require('path');
-//app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/index.html'));
   res.status(200);
+});
+
+app.get('/user/new', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/user/new.html'));
 });
 
 var server = app.listen(port, function() {
