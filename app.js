f92156e1af17de005a8a211b4b85bf8e00e0f4c9
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-08 13:56:25 +0000

    Update entry point to app for new view engine and routing.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var path = require('path');
+var index = require('./app_server/routes/index');
+var user = require('./app_server/routes/user');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'pug');
+
+app.use('/', index);
+app.use('/user', user);
 
 module.exports = app;
