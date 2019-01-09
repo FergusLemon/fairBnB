c389b0a8294de178222ae26e9b80ec1ec72baef8
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-09 12:12:27 +0000

    Refactor - better paramater name for User constructor.

--- src/user.js
+++ src/user.js
@@ -1,16 +1,17 @@
 'use strict';
 
-function User(obj) {
+function User(userDetails) {
 
-  this.invalidUserDetails = function() {
-    if(!obj || !obj.username || !obj.firstname || !obj.lastname) return true;
+  this.invalidDetails = function() {
+    if(!userDetails || !userDetails.username ||
+    !userDetails.firstname || !userDetails.lastname) return true;
   };
 
-  if(this.invalidUserDetails()) throw new Error("A user must have a username,\
+  if(this.invalidDetails()) throw new Error("A user must have a username,\
  first name and a last name.");
 
   this.getUsername = function() {
-    return obj.username;
+    return userDetails.username;
   };
 
 }
