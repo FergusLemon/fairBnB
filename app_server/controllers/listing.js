b87e311d6f205d906fa04cbcb153c01599706b11
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-10 19:07:03 +0000

    Add a link to view listings in the layout template, add dummy data for testing, write new test that expects all listings to be shown when a user clicks on view listings.

--- app_server/controllers/listing.js
+++ app_server/controllers/listing.js
@@ -4,10 +4,18 @@ module.exports.new = function(req, res, next) {
 
 module.exports.listings = function(req, res, next) {
   res.render('listings/all', {
-    property: {
+    properties: [{
       name: req.body.name,
       description: req.body.description,
       price: req.body.price
-    }
+    }, {
+      name: "Dummy Casa 1",
+      description: "A great first dummy casa with fantastic views.",
+      price: "100"
+    }, {
+      name: "Dummy Casa 2",
+      description: "A cosy second dummy casa with superb amenities.",
+      price: "50"
+    }]
   });
 };
--- test/feature/listing/listingsSpec.js
+++ test/feature/listing/listingsSpec.js
@@ -10,15 +10,21 @@ fixture `Listings Page`
 
   test('There is a listing on the page', async t => {
     await t
-      .expect(Selector('#1')).exists;
+      .expect(Selector('#name-0').exists).ok();
   });
 
   test('The listing is the one the user created', async t => {
-    const name = Selector('#name');
-    const description = Selector('#description');
-    const price = Selector('#price');
+    const name = Selector('#name-0');
+    const description = Selector('#description-0');
+    const price = Selector('#price-0');
     await t
       .expect(name.innerText).eql('Name: Casa Test')
       .expect(description.innerText).eql('Description: A nice test casa.')
       .expect(price.innerText).eql('Price Per Night: 100');
   });
+
+  test("When there other listings, the user can see those also", async t => {
+    await t
+      .expect(Selector('#name-1').exists).ok()
+      .expect(Selector('#name-2').exists).ok();
+  })
