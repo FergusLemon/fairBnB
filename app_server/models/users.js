0c4e2ba82a48f6c15401a3d1c3297fcf0ad9f108
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-14 17:02:55 +0000

    Update user schema path for phone numbers.

--- app_server/models/users.js
+++ app_server/models/users.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   firstname: { type: String, required: true },
-  lastname: { type: String, required: true }
+  lastname: { type: String, required: true },
+  phoneNumber: { type: String }
 });
 
 module.exports = mongoose.model('User', userSchema);
