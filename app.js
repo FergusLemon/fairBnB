b212e7d55817c1cac9364a78ad1dfbb34fd46593
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-16 12:39:51 +0000

    Update file path for db file now it is in the api directory.

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
-var index = require('./app_server/routes/index');
+var server = require('./app_server/routes/index');
+var api = require('./app_api/routes/index');
 var user = require('./app_server/routes/user');
 var listing = require('./app_server/routes/listing');
 var session = require('./app_server/routes/session');
-require('./app_server/models/db');
+require('./app_api/models/db');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
-app.use('/', index);
+app.use('/', server);
+app.use('/api', api);
 app.use('/users', user);
 app.use('/listings', listing);
 app.use('/session', session);
