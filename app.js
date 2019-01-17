7a77c0eb3ae5382b92386ad46ad9c6bd42d73ab4
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-17 11:32:21 +0000

    Use const keyword.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var path = require('path');
-var server = require('./app_server/routes/index');
-var api = require('./app_api/routes/index');
-var user = require('./app_server/routes/user');
-var listing = require('./app_server/routes/listing');
-var session = require('./app_server/routes/session');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+const server = require('./app_server/routes/index');
+const apiUser = require('./app_api/routes/users');
+const user = require('./app_server/routes/user');
+const listing = require('./app_server/routes/listing');
+const session = require('./app_server/routes/session');
 require('./app_api/models/db');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 app.use('/', server);
-app.use('/api', api);
+app.use('/api/users', apiUser);
 app.use('/users', user);
 app.use('/listings', listing);
 app.use('/session', session);
