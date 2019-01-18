92186f8079c0325ffc21d7e4301979620e758814
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-18 12:04:30 +0000

    Update feature tests and view to include a phone number field and remove commented out code from api user controller.

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const User = require('../models/users');
 
 let sendJsonResponse = function(res, status, content) {
-  console.log(status);
-  console.log(content);
   res.status(status);
   res.json(content);
 };
@@ -20,25 +18,3 @@ module.exports.createUser = (req, res) => {
     }
   });
 };
-//module.exports.getUser = function(req, res) {
-//  if (req.params && req.params.userid) {
-//    User
-//      .findById(req.params.userid)
-//      .exec((err, user) => {
-//        if(!user) {
-//          sendJsonResponse(res, 404, {
-//            "message": "Sorry this user cannot be found."
-//          });
-//          return;
-//        } else if (err) {
-//          sendJsonResponse(res, 404, err);
-//          return;
-//        }
-//        sendJsonResponse(res, 200, user);
-//      });
-//    } else {
-//      sendJsonResponse(res, 404, {
-//        "message": "No User ID was sent with the request."
-//      });
-//    }
-//};
