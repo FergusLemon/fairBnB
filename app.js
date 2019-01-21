7f7ef44e3ea317f595691793287fca004882214e
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-21 18:55:49 +0000

    Add Passport.js, flash and use sessions, all for user authentication / login.

--- app.js
+++ app.js
@@ -9,11 +9,22 @@ const api = require('./app_api/routes/index');
 const apiUser = require('./app_api/routes/users');
 const user = require('./app_server/routes/user');
 const listing = require('./app_server/routes/listing');
-const session = require('./app_server/routes/session');
+const changeThis = require('./app_server/routes/session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require('connect-flash');
+const session = require('express-session');
+const { secret } = require('./config');
 require('./app_api/models/db');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(session({ cookie: { maxAge: 60000 },
+                  secret: process.env.SESSION_SECRET,
+                  resave: false,
+                  saveUninitialized: false}));
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
@@ -22,6 +33,6 @@ app.use('/api', api);
 app.use('/api/users', apiUser);
 app.use('/users', user);
 app.use('/listings', listing);
-app.use('/session', session);
+app.use('/session', changeThis);
 
 module.exports = app;
