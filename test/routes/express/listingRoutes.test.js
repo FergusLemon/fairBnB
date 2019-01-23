b6995f0a82c89b6df91e6019591836b7d6e87d85
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-23 15:20:21 +0000

    Unit test on the express route for a new listing to test handling of status codes back from the API.

--- test/routes/express/listingRoutes.test.js
+++ test/routes/express/listingRoutes.test.js
@@ -14,9 +14,12 @@ const Factory = require(path.join(HOMEDIR, 'test', 'helpers', 'factories'));
 chai.use(sinonChai);
 
 describe('creating a listing', () => {
+  let listingDetails, req;
+  beforeEach(() => {
+    listingDetails = Factory.validListing();
+    req = { body: listingDetails };
+  });
   it('makes a post request to the API wrapping the DB', sandboxed(function() {
-    let listingDetails = Factory.validListing();
-    let req = { body: listingDetails };
     let res = {
       sendCalledWith: '',
       send: function(arg) {
@@ -26,5 +29,33 @@ describe('creating a listing', () => {
     this.stub(request, 'post');
     ListingController.addListing(req, res);
     expect(request.post).to.have.been.calledOnce;
+  }));
+  it('responds with an error if it is passed a 403 status code back from the API', sandboxed(function() {
+    let res = {
+      sendCalledWith: '',
+      send: function(arg) {
+        this.sendCalledWith = arg;
+      }
+    };
+    this.stub(request, 'post').yields(null, { statusCode: 403 });
+    ListingController.addListing(req, res);
+    expect(res.sendCalledWith).to.contain('Something went wrong');
+  }));
+  it('does not send an error if it is passed a 201 status code', sandboxed(function() {
+    let res = {
+      sendCalledWith: '',
+      send: function(arg) {
+        this.sendCalledWith = arg;
+      },
+      redirectCalledWith: '',
+      redirect: function(arg) {
+        this.redirectCalledWith = arg;
+      }
+    };
+    this.stub(request, 'post').yields(null, { statusCode: 201 }, { ownerID: "stubbedOwnerID" });
+    ListingController.addListing(req, res);
+    expect(res.sendCalledWith).to.contain('');
+    expect(res.redirectCalledWith).to.contain('users/stubbedOwnerID');
+    expect(res.redirectCalledWith).to.not.contain('Something went wrong');
   }));
 });
