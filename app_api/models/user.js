00da56d1ebf6cb0a3a06c0d0d527349a5a868716
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-28 20:28:37 +0000

    Add chai library for testing rejected promises, test rejected promise when comparing user passwords on login.

--- app_api/models/user.js
+++ app_api/models/user.js
@@ -14,13 +14,10 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', function(next) {
   let user = this;
-
   bcrypt.genSalt(saltRounds, function(err, salt) {
     if (err) return next(err);
-
     bcrypt.hash(user.password, salt, function(err, hash) {
       if (err) return next(err);
-
       user.password = hash;
       next();
     });
