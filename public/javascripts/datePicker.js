c6f0a2a07f8926510dbf90c120c02b627ca44a28
Fergus Lemon <fergus.lemon@gmail.com>
2019-02-05 17:56:15 +0000

    Update date picker plug in to check for and block out certain dates - skeleton to be hooked up to the unavailableDates array for a listing.

--- public/javascripts/datePicker.js
+++ public/javascripts/datePicker.js
@@ -1,9 +1,17 @@
 'use strict';
 $(document).ready(function () {
+  console.log(window.location.pathname);
   $('input[name="dates"]').daterangepicker({
     "locale": {
         "format": "YYYY-MM-DD",
         "separator": " - "
+      },
+      "isInvalidDate": function(date) {
+        let unavailableDates = ['2019-02-21'];
+        console.log(date.format('YYYY-MM-DD'));
+        let formattedDate = date.format('YYYY-MM-DD');
+        console.log(unavailableDates.includes(formattedDate));
+        return unavailableDates.includes(formattedDate);
       }
   });
 });
