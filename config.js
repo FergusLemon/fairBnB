0941f662aeb36cccdd487c4a5dd2a2fcbeff3e83
Fergus Lemon <fergus.lemon@gmail.com>
2019-02-07 10:43:37 +0000

    Add env vars for cloud storage.

--- config.js
+++ config.js
@@ -5,5 +5,10 @@ dotenv.config();
 module.exports = {
   environment: process.env.NODE_ENV,
   server: process.env.SERVER,
-  secret: process.env.SESSION_SECRET
+  secret: process.env.SESSION_SECRET,
+  cloudName: process.env.CLOUD_NAME,
+  cloudApi: process.env.CLOUD_API,
+  cloudSecret: process.env.CLOUD_SECRET,
+  stockImageUrl: process.env.URL,
+  stockImageId: process.env.PUBLIC_ID
 };
