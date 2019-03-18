6b2cfc1104a1ace208a6754d5a9266196b5900e1
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-18 10:37:38 +0000

    Reduce line length.

--- public/javascripts/passwordChecker.js
+++ public/javascripts/passwordChecker.js
@@ -7,7 +7,9 @@ window.addEventListener("load", function(event) {
     passwordInput.addEventListener("blur", function(event) {
       let password = passwordInput.value;
       if (!password.match(regex)) {
-        toDisplay.textContent = "Your password must be between 8-32 characters long and contain at least 1 lowercase letter, uppercase letter, number and symbol.";
+          toDisplay.textContent = `Your password must be between 8-32 characters
+                                  long and contain at least 1 lowercase letter,
+                                  uppercase letter, number and symbol.`;
       } else {
         toDisplay.textContent = "";
       }
